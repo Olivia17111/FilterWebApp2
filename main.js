13082ecdc3de76b5cc7bf000b18d53bd0e23a385
Olivia17111 <99030869+Olivia17111@users.noreply.github.com>
2022-06-25 19:11:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
+nosex = 0;
+nosey = 0;
+Mustache = 0;
 function preload() {
-
+if(Mustache==0)
+{
+clone_mustache = loadImage("https://i.postimg.cc/pLRYXLB7/mustache-cartoon-isolated-vector-23490826-removebg-preview.png");
+}
+else if (Mustache == 1) 
+{
+    clone_mustache = loadImage("https://i.postimg.cc/pLRYXLB7/mustache-cartoon-isolated-vector-23490826-removebg-preview.png");
+}
 }
 
 function setup() {
-canvas = createCanvas(300,300);
+canvas = createCanvas(300,400);
 canvas.center();
 video = createCapture(VIDEO);
-video.size(300,300);
+video.size(300,400);
 video.hide();
 posenet = ml5.poseNet(video,modelLoaded);
 posenet.on('pose',gotPoses);
@@ -18,20 +28,23 @@ posenet.on('pose',gotPoses);
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
-        console.log("nosex = " + results[0].pose.nose.x);
-        console.log( "nosey = " + results[0].pose.nose.y);
+         nosex =  results[0].pose.nose.x -40;
+         nosey =  results[0].pose.nose.y +1;
+         console.log("nosex= " + nosex);
+         console.log("nosey= " + nosey);
     }
 } 
 
 
 function draw() {
-    image(video,0,0,300,300);
+    image(video,0,0,300,400);
+    image(clone_mustache,nosex,nosey,80,80);
 
+ //fill (255,0,0);
+ //stroke (255,1,0);
+ //circle(nosex,nosey,30);
 }
 
 function take_snapshot() {
-    save('redNose.png');
+    save('MyMustache.png');
 }
-
-
-
